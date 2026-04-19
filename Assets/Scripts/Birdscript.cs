19b054b3cbfeef396a65f5f60a47e0b61dfb5d0c
moroza27 <moroz.a_ipz23@rcit.ukr.education>
2026-04-19 23:52:54 +0300

    3

--- Assets/Scripts/Birdscript.cs
+++ Assets/Scripts/Birdscript.cs
@@ -3,20 +3,33 @@ using UnityEngine;
 public class Birdscript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
-    public float flapStrength = 10;
+    public float flapStrength = 5;
+    public LogicScript logic; // Посилання на наш менеджер
+    public bool birdIsAlive = true;
 
     void Start()
     {
         // Пташка просто бере своє тіло. Ніякого пошуку логіки.
         myRigidbody = GetComponent<Rigidbody2D>();
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     void Update()
     {
-        // Стрибок
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        // Пташка може стрибати ТІЛЬКИ якщо вона жива
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
+        }
+    }
+    // Ця функція спрацьовує при зіткненні "контейнерів" (колайдерів)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Якщо ми врізалися в об'єкт з тегом "Enemy" або об'єкт на шарі "Obstacle"
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            birdIsAlive = false; // Пташка вмирає
+            logic.gameOver();    // Викликаємо зупинку гри та екран Game Over
         }
     }
 }
