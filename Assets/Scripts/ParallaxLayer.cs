0cddf5fdc7d27bf13c841616d69dff654c00d154
Xsessha <olkhovskayxseniia@gmail.com>
2026-05-09 13:26:15 +0300

    save

---
+++ Assets/Scripts/ParallaxLayer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float parallaxFactor = 0.3f;
+
+    private Transform cam;
+    private Vector3 lastCamPos;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+        lastCamPos = cam.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 delta = cam.position - lastCamPos;
+
+        transform.position += new Vector3(delta.x * parallaxFactor, delta.y * parallaxFactor, 0);
+
+        lastCamPos = cam.position;
+    }
+}
